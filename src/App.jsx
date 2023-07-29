1c2337a0de2238ca6522073a99c83411c6c6da6a
Htetaungkyaw71 <htetaung200071@gmail.com>
2023-07-29 22:45:19 +0630

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import Detail from "./components/Detail";
-import Home from "./components/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Preview from "./components/Preview";
+
+import { Suspense, lazy } from "react";
+import img from "./assets/link.png";
+
+const Home = lazy(() => import("./components/Home"));
+const Detail = lazy(() => import("./components/Detail"));
+const Preview = lazy(() => import("./components/Preview"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/detail" element={<Detail />} />
-        <Route path="/:id" element={<Preview />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center pt-60">
+            <img src={img} alt="loader" className="w-10 h-10 animate-spin" />
+          </div>
+        }
+      >
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail" element={<Detail />} />
+          <Route path="/:id" element={<Preview />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
